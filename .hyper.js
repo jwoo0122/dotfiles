cf9843da0ff1acb70db39a0f01032edbd9057972
Jinwoo Jeong <thinkratomos@gmail.com>
2020-11-15 21:39:02 +0900

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 15,
 
     // font family with optional fallbacks
-    fontFamily: 'MesloLGLDZ Nerd Font Mono',
+    fontFamily: 'MesloLGL Nerd Font',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
