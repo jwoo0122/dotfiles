e9982d87d58e11e9d4f6ce0d7ca90efc93c56f9e
정진우 <leo@toss.im>
2024-04-06 15:05:20 +0900

    hyper default shell

--- dot_hyper.js
+++ dot_hyper.js
@@ -140,7 +140,7 @@ module.exports = {
     //
     // Cygwin
     // - Example: `C:\\cygwin64\\bin\\bash.exe`
-    shell: "",
+    shell: "/bin/zsh",
     // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
     // by default `['--login']` will be used
     shellArgs: ["--login"],
