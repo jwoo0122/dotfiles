fad663879bf18d94532b16c3fb1ba1a974c40a86
Jinwoo Jeong <thinkratomos@gmail.com>
2024-09-06 02:59:29 +0900

    add more

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,7 @@
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.g.mapleader = " "
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
@@ -9,4 +13,34 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require('kanagawa').setup({
+				theme = "wave"
+			})
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
+	{
+		'nvim-telescope/telescope.nvim',
+		event = 'VimEnter',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		config = function()
+			require('telescope').setup({})
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+			vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+			vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, {})
+		end
+	},
+	{
+		'folke/flash.nvim',
+		event = 'VeryLazy',
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end }
+		}
+	}
 })
