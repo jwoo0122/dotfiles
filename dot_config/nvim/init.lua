4bfeb44909d0c8174fe4d8e0edb35da0e56523a7
Jinwoo Jeong <thinkratomos@gmail.com>
2024-12-13 03:03:23 +0900

    add more

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -131,6 +131,43 @@ require("lazy").setup({
         show_hidden = true
       }
     },
+  },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+    },
+    config = function()
+      local cmp = require('cmp')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end
+        },
+        mapping = {
+          ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+              cmp.confirm({ select = true })
+            else
+              fallback()
+            end
+          end,
+          ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+          ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+        }),
+        {
+          { name = 'buffer' }
+        }
+      })
+    end
   }
 })
 
