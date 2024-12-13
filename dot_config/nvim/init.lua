7fdf300d4b31780d1b54f4b6c0ee05d61c778f8d
Jinwoo Jeong <thinkratomos@gmail.com>
2024-12-14 01:11:54 +0900

    add more

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -78,6 +78,7 @@ require("lazy").setup({
       require('telescope').setup({})
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, {})
@@ -201,4 +202,5 @@ require("lazy").setup({
     end
   },
   { 'echasnovski/mini.statusline', version = false, config = true },
+  { 'nvim-tree/nvim-web-devicons', config = true },
 })
