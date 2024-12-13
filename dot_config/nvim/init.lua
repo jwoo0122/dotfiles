ef9dd444034a4c1883f97164f800927d32ff0853
Jinwoo Jeong <thinkratomos@gmail.com>
2024-12-14 01:50:33 +0900

    add more

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -75,7 +75,13 @@ require("lazy").setup({
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-      require('telescope').setup({})
+      require('telescope').setup({
+        pickers = {
+          find_files = {
+            hidden = true
+          }
+        }
+      })
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
