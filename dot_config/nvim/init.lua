6d2421a859417428af16657340b259cdd0723155
Jinwoo Jeong <thinkratomos@gmail.com>
2024-12-14 11:34:59 +0900

    add more

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -95,6 +95,7 @@ require("lazy").setup({
     event = 'VeryLazy',
     keys = {
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end },
     },
   },
   {
