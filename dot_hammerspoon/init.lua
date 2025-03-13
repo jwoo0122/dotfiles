5bd5a3818c73fe2bcffea0ec6a6079f7109cb0cc
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-03-13 22:18:42 +0900

    Update .hammerspoon/init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -10,14 +10,14 @@ end
 
 --- quick open applications
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["h"], open_app("ghostty"))
-hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["v"], open_app("Cursor"))
+-- hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["v"], open_app("Cursor"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["w"], open_app("Safari"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["s"], open_app("Slack"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["f"], open_app("Finder"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["t"], open_app("Telegram"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["l"], open_app("Linear"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["c"], open_app("Google Chrome"))
-hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["n"], open_app("Notion"))
+hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["n"], open_app("Notes"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["x"], open_app("xcode"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["m"], open_app("Mail"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["p"], open_app("Perplexity"))
