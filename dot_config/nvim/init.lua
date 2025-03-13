b8292f207a6f4bb4679c4bb02c06840dd441ed07
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-03-14 00:58:52 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -247,13 +247,7 @@ require("lazy").setup({
   {
     'nvim-lualine/lualine.nvim',
     config = function()
-      require('lualine').setup({
-        options = {
-          theme = 'auto',
-          component_separators = { left = '', right = '' },
-          section_separators = { left = '', right = '' },
-        }
-      })
+      require('lualine').setup({})
     end
   },
   {
