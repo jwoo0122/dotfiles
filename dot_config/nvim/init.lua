935999b7a7c6b0fae7f6eab659630c09ed5a728c
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-03-17 00:29:41 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -231,7 +231,13 @@ require("lazy").setup({
   },
   {
     'stevearc/oil.nvim',
-    opts = {}
+    config = function()
+      require('oil').setup {
+        view_options = {
+          show_hidden = true,
+        }
+      }
+    end
   },
   {
     'akinsho/toggleterm.nvim',
