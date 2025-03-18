4f32c80c82ae32e0c4f74332ebb0b62338a24a25
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-03-18 13:38:37 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -325,10 +325,14 @@ require("lazy").setup({
     },
   },
   {
-    "nvim-neorg/neorg",
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = "*", -- Pin Neorg to the latest stable release
-    config = true,
+    "nvim-orgmode/orgmode",
+    event = 'VeryLazy',
+    config = function()
+      require('orgmode').setup {
+        org_agenda_files = '~/orgfiles/**/*',
+        org_default_notes_file = '~/orgfiles/refile.org',
+      }
+    end,
   },
   {
     'MeanderingProgrammer/render-markdown.nvim',
