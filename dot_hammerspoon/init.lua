0963ca949d386370e075df82819b1aa2ed718c3e
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-05-08 20:39:26 +0900

    Update .hammerspoon/init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,16 +1,16 @@
 -- start quick open applications
 local function open_app(name)
-	return function()
-		hs.application.launchOrFocus(name)
-		if name == "Finder" then
-			hs.appfinder.appFromName(name):activate()
-		end
-	end
+  return function()
+    hs.application.launchOrFocus(name)
+    if name == "Finder" then
+      hs.appfinder.appFromName(name):activate()
+    end
+  end
 end
 
 --- quick open applications
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["h"], open_app("ghostty"))
-hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["v"], open_app("Cursor"))
+-- hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["v"], open_app("Cursor"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["w"], open_app("Safari"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["s"], open_app("Slack"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["f"], open_app("Finder"))
@@ -20,29 +20,29 @@ hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["c"], open_app("Google Chr
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["n"], open_app("Notes"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["x"], open_app("xcode"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["m"], open_app("Mail"))
-hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["z"], open_app("zed"))
+hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["v"], open_app("zed"))
 -- hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["p"], open_app("Perplexity"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["d"], open_app("Claude"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["p"], open_app("Perplexity"))
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["j"], open_app("IntelliJ IDEA CE"))
 
 hs.hotkey.bind({ "option", "shift" }, hs.keycodes.map["o"], function()
-	local clicked, query = hs.dialog.textPrompt("Search on web", "", "", "Search", "Cancel")
-	if clicked == "Cancel" then
-		return
-	end
-	local query_encoded = hs.http.encodeForQuery(query)
-	hs.execute("open " .. "https://duckduckgo.com/?q=" .. query_encoded)
+  local clicked, query = hs.dialog.textPrompt("Search on web", "", "", "Search", "Cancel")
+  if clicked == "Cancel" then
+    return
+  end
+  local query_encoded = hs.http.encodeForQuery(query)
+  hs.execute("open " .. "https://duckduckgo.com/?q=" .. query_encoded)
 end)
 
 -- Window management
 -- half of screen
 -- https://martinlwx.github.io/en/how-to-manage-windows-using-hammerspoon/
-hs.hotkey.bind({'ctrl', 'option'}, 'left', function() hs.window.focusedWindow():moveToUnit({0, 0, 0.5, 1}) end)
-hs.hotkey.bind({'ctrl', 'option'}, 'right', function() hs.window.focusedWindow():moveToUnit({0.5, 0, 0.5, 1}) end)
-hs.hotkey.bind({'ctrl', 'option'}, 'up', function() hs.window.focusedWindow():moveToUnit({0, 0, 1, 0.5}) end)
-hs.hotkey.bind({'ctrl', 'option'}, 'down', function() hs.window.focusedWindow():moveToUnit({0, 0.5, 1, 0.5}) end)
+hs.hotkey.bind({ 'ctrl', 'option' }, 'left', function() hs.window.focusedWindow():moveToUnit({ 0, 0, 0.5, 1 }) end)
+hs.hotkey.bind({ 'ctrl', 'option' }, 'right', function() hs.window.focusedWindow():moveToUnit({ 0.5, 0, 0.5, 1 }) end)
+hs.hotkey.bind({ 'ctrl', 'option' }, 'up', function() hs.window.focusedWindow():moveToUnit({ 0, 0, 1, 0.5 }) end)
+hs.hotkey.bind({ 'ctrl', 'option' }, 'down', function() hs.window.focusedWindow():moveToUnit({ 0, 0.5, 1, 0.5 }) end)
 
 -- full screen
-hs.hotkey.bind({'ctrl', 'option'}, 'return', function() hs.window.focusedWindow():moveToUnit({0, 0, 1, 1}) end)
-hs.hotkey.bind({'ctrl', 'option'}, 'c', function() hs.window.focusedWindow():centerOnScreen() end)
+hs.hotkey.bind({ 'ctrl', 'option' }, 'return', function() hs.window.focusedWindow():moveToUnit({ 0, 0, 1, 1 }) end)
+hs.hotkey.bind({ 'ctrl', 'option' }, 'c', function() hs.window.focusedWindow():centerOnScreen() end)
