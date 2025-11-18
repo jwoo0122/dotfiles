9f8b5967aad67f1b51e34aec8ffa071dabeb3954
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-11-18 21:38:40 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -302,25 +302,5 @@ require("lazy").setup({
     "tpope/vim-fugitive",
     config = function()
     end
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-    config = function()
-      require('neo-tree').setup({
-        filesystem = {
-          window = {
-            position = "left"
-          }
-        },
-        reveal = true,
-      })
-    end
   }
 })
