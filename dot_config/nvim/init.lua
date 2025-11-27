d57dc36d1eb5bf55982f0fece44a190c56985a37
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-11-27 13:26:35 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -310,7 +310,11 @@ require("lazy").setup({
     lazy = false, -- neo-tree will lazily load itself
     config = function()
       require('neo-tree').setup({
-        position = 'current'
+        filesystem = {
+          window = {
+            position = 'current'
+          }
+        }
       })
     end
   }
