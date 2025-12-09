a411a0cb4aa4234e86806f85fb66c13c15093c8a
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-12-09 14:15:52 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -12,7 +12,7 @@ vim.opt.list = true
 vim.opt.cursorline = true
 
 vim.opt.listchars:append {
-  tab = "|-",
+  tab = "┊ ",
   multispace = "·",
   lead = "·",
   trail = "·",
@@ -49,15 +49,22 @@ vim.keymap.set('n', 'ge', vim.diagnostic.open_float)
 vim.diagnostic.config({
   float = {
     source = true,
-    border = 'single',
+    border = 'rounded',
   }
 })
 
+-- hover
+local originalHover = vim.lsp.buf.hover
+vim.lsp.buf.hover = function()
+  return originalHover({
+    border = "rounded",
+    max_width = math.floor(vim.o.columns * 0.7),
+    max_height = math.floor(vim.o.lines * 0.7)
+  })
+end
+
 --- netrw
--- vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
-
-
 
 -- Window
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
@@ -186,17 +193,6 @@ require("lazy").setup({
     event = 'InsertEnter',
     config = true,
   },
-  -- {
-  --   'nvim-lualine/lualine.nvim',
-  --   config = function()
-  --     require('lualine').setup {
-  --       options = {
-  --         component_separators = { left = ' ', right = ' ' },
-  --         section_separators = { left = ' ', right = ' ' },
-  --       }
-  --     }
-  --   end
-  -- },
   {
     'nvim-treesitter/nvim-treesitter',
     config = function()
@@ -221,23 +217,6 @@ require("lazy").setup({
     end
   },
   {
-    'Bekaboo/dropbar.nvim',
-    config = function()
-      require('dropbar').setup({
-        menu = {
-          preview = false,
-          win_configs = {
-            border = 'rounded'
-          }
-        }
-      })
-      local dropbar_api = require('dropbar.api')
-      vim.keymap.set('n', '<leader>;', dropbar_api.pick)
-      vim.keymap.set('n', '[;', dropbar_api.goto_context_start)
-      vim.keymap.set('n', '];', dropbar_api.select_next_context)
-    end
-  },
-  {
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
     dependencies = {
@@ -248,8 +227,17 @@ require("lazy").setup({
       local telescope = require('telescope')
       telescope.setup({
         defaults = {
-          layout_strategy = 'vertical',
-          preview = false,
+          layout_strategy = 'horizontal',
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              width = { padding = 0 },
+              height = { padding = 0 },
+              preview_width = 0.5
+            }
+          },
+          preview = true,
+          sorting_strategy = "ascending"
         },
         extensions = {
           fzf = {
@@ -314,74 +302,6 @@ require("lazy").setup({
     end
   },
   {
-    "jake-stewart/multicursor.nvim",
-    branch = "1.0",
-    config = function()
-        local mc = require("multicursor-nvim")
-        mc.setup()
-
-        local set = vim.keymap.set
-
-        -- Add or skip cursor above/below the main cursor.
-        set({"n", "x"}, "<up>", function() mc.lineAddCursor(-1) end)
-        set({"n", "x"}, "<down>", function() mc.lineAddCursor(1) end)
-        set({"n", "x"}, "<leader><up>", function() mc.lineSkipCursor(-1) end)
-        set({"n", "x"}, "<leader><down>", function() mc.lineSkipCursor(1) end)
-
-        -- Add or skip adding a new cursor by matching word/selection
-        set({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end)
-        set({"n", "x"}, "<leader>s", function() mc.matchSkipCursor(1) end)
-        set({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end)
-        set({"n", "x"}, "<leader>S", function() mc.matchSkipCursor(-1) end)
-
-        -- Add and remove cursors with control + left click.
-        set("n", "<c-leftmouse>", mc.handleMouse)
-        set("n", "<c-leftdrag>", mc.handleMouseDrag)
-        set("n", "<c-leftrelease>", mc.handleMouseRelease)
-
-        -- Disable and enable cursors.
-        set({"n", "x"}, "<c-q>", mc.toggleCursor)
-
-        -- Mappings defined in a keymap layer only apply when there are
-        -- multiple cursors. This lets you have overlapping mappings.
-        mc.addKeymapLayer(function(layerSet)
-
-            -- Select a different cursor as the main one.
-            layerSet({"n", "x"}, "<left>", mc.prevCursor)
-            layerSet({"n", "x"}, "<right>", mc.nextCursor)
-
-            -- Delete the main cursor.
-            layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
-
-            -- Enable and clear cursors using escape.
-            layerSet("n", "<esc>", function()
-                if not mc.cursorsEnabled() then
-                    mc.enableCursors()
-                else
-                    mc.clearCursors()
-                end
-            end)
-        end)
-
-        -- Customize how cursors look.
-        local hl = vim.api.nvim_set_hl
-        hl(0, "MultiCursorCursor", { reverse = true })
-        hl(0, "MultiCursorVisual", { link = "Visual" })
-        hl(0, "MultiCursorSign", { link = "SignColumn"})
-        hl(0, "MultiCursorMatchPreview", { link = "Search" })
-        hl(0, "MultiCursorDisabledCursor", { reverse = true })
-        hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-        hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
-    end
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
-  },
-  {
     "gelguy/wilder.nvim",
     config = function()
       local wilder = require('wilder')
@@ -394,5 +314,50 @@ require("lazy").setup({
         highlights = {default = "WilderMenu", accent = "WilderAccent"}
       }))
     end
-  }
+  },
+  {
+    'kevinhwang91/nvim-bqf',
+    config = function()
+    end
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+      require('toggleterm').setup{
+        open_mapping = [[<c-\>]]
+      }
+    end
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    config = function()
+      local highlight = {
+            "CursorColumn",
+            "Whitespace",
+          }
+      require('ibl').setup {
+        indent = {
+          highlight = highlight,
+          char = ""
+        },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
+        scope = {
+          enabled = true
+        }
+      }
+    end
+  },
+  {
+    'prichrd/netrw.nvim',
+    opts = {}
+  },
+  { "nvim-tree/nvim-web-devicons", opts = {} },
+  { "nvim-lualine/lualine.nvim" }
 })
