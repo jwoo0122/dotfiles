5485f9ae4bef9c9bbfd7df27069cead62fbf178c
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-12-09 14:19:43 +0900

    Update .config/ghostty/config Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -96,7 +96,7 @@ require("lazy").setup({
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd('colorscheme tokyonight-night')
+      vim.cmd('colorscheme tokyonight-moon')
     end
   },
   {
@@ -359,5 +359,66 @@ require("lazy").setup({
     opts = {}
   },
   { "nvim-tree/nvim-web-devicons", opts = {} },
-  { "nvim-lualine/lualine.nvim" }
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      local colors = {
+        blue   = '#80a0ff',
+        cyan   = '#79dac8',
+        black  = '#080808',
+        white  = '#c6c6c6',
+        red    = '#ff5189',
+        violet = '#d183e8',
+        grey   = '#303030',
+      }
+
+      local bubbles_theme = {
+        normal = {
+          a = { fg = colors.black, bg = colors.violet },
+          b = { fg = colors.white, bg = colors.grey },
+          c = { fg = colors.white },
+        },
+
+        insert = { a = { fg = colors.black, bg = colors.blue } },
+        visual = { a = { fg = colors.black, bg = colors.cyan } },
+        replace = { a = { fg = colors.black, bg = colors.red } },
+
+        inactive = {
+          a = { fg = colors.white, bg = colors.black },
+          b = { fg = colors.white, bg = colors.black },
+          c = { fg = colors.white },
+        },
+      }
+
+      require('lualine').setup {
+        options = {
+          theme = bubbles_theme,
+          component_separators = '',
+          section_separators = { left = '', right = '' },
+        },
+        sections = {
+          lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
+          lualine_b = { 'filename', 'branch' },
+          lualine_c = {
+            '%=', --[[ add your center components here in place of this comment ]]
+          },
+          lualine_x = {},
+          lualine_y = { 'filetype', 'progress' },
+          lualine_z = {
+            { 'location', separator = { right = '' }, left_padding = 2 },
+          },
+        },
+        inactive_sections = {
+          lualine_a = { 'filename' },
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { 'location' },
+        },
+        tabline = {},
+        extensions = {},
+      }
+    end
+  }
 })
