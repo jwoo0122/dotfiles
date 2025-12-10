b63ee4b997c80e5db106eab6476f3380f4613722
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2025-12-10 21:36:20 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -25,6 +25,9 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.inccommand = "split"
 
+-- statusline
+vim.opt.laststatus = 3
+
 ---Split direction
 vim.opt.splitright = true
 vim.opt.splitbelow = true
@@ -97,6 +100,34 @@ require("lazy").setup({
     priority = 1000,
     config = function()
       vim.cmd('colorscheme tokyonight-moon')
+
+      local colors = require("tokyonight.colors").setup()
+      local tn_util = require("tokyonight.util")
+
+      local mode_colors = {
+        n = colors.blue,
+        i = colors.orange,
+        v = colors.purple,
+        V = colors.purple,
+        ["\22"] = colors.purple,
+        c = colors.red
+      }
+
+      local function setColor(mode)
+        local statusline_color = tn_util.darken(mode_colors[mode] or mode_colors["n"], 0.7)
+        vim.api.nvim_set_hl(0, "StatusLine", { bg = statusline_color, fg = "#111111", bold = true })
+      end
+
+      vim.api.nvim_create_autocmd("ModeChanged", {
+        pattern = "*",
+        callback = function()
+          local mode = vim.fn.mode()
+
+          setColor(mode)
+        end
+      })
+
+      setColor('n')
     end
   },
   {
@@ -307,12 +338,17 @@ require("lazy").setup({
       local wilder = require('wilder')
 
       wilder.setup({ modes = { ":", "/", "?" }})
-      wilder.set_option("renderer", wilder.popupmenu_renderer({
-        highlighter = wilder.basic_highlighter(),
-        left = {" "},
-        right = {" ", wilder.popupmenu_scrollbar({ thumb_char = " " })},
-        highlights = {default = "WilderMenu", accent = "WilderAccent"}
-      }))
+      wilder.set_option("renderer", wilder.popupmenu_renderer(
+        wilder.popupmenu_palette_theme({
+          highlighter = wilder.basic_highlighter(),
+          border = 'rounded',
+          max_width = '75%',
+          reverse = 0,
+          left = {' ', wilder.popupmenu_devicons()},
+          right = {' ', wilder.popupmenu_scrollbar()},
+          prompt_position = 'bottom',
+        })
+      ))
     end
   },
   {
@@ -359,66 +395,4 @@ require("lazy").setup({
     opts = {}
   },
   { "nvim-tree/nvim-web-devicons", opts = {} },
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      local colors = {
-        blue   = '#80a0ff',
-        cyan   = '#79dac8',
-        black  = '#080808',
-        white  = '#c6c6c6',
-        red    = '#ff5189',
-        violet = '#d183e8',
-        grey   = '#303030',
-      }
-
-      local bubbles_theme = {
-        normal = {
-          a = { fg = colors.black, bg = colors.violet },
-          b = { fg = colors.white, bg = colors.grey },
-          c = { fg = colors.white },
-        },
-
-        insert = { a = { fg = colors.black, bg = colors.blue } },
-        visual = { a = { fg = colors.black, bg = colors.cyan } },
-        replace = { a = { fg = colors.black, bg = colors.red } },
-
-        inactive = {
-          a = { fg = colors.white, bg = colors.black },
-          b = { fg = colors.white, bg = colors.black },
-          c = { fg = colors.white },
-        },
-      }
-
-      require('lualine').setup {
-        options = {
-          theme = bubbles_theme,
-          component_separators = '',
-          section_separators = { left = '', right = '' },
-        },
-        sections = {
-          lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-          lualine_b = { 'filename', 'branch' },
-          lualine_c = {
-            '%=', --[[ add your center components here in place of this comment ]]
-          },
-          lualine_x = {},
-          lualine_y = { 'filetype', 'progress' },
-          lualine_z = {
-            { 'location', separator = { right = '' }, left_padding = 2 },
-          },
-        },
-        inactive_sections = {
-          lualine_a = { 'filename' },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {},
-          lualine_z = { 'location' },
-        },
-        tabline = {},
-        extensions = {},
-      }
-    end
-  }
 })
