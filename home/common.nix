ae0d6167dbc04b9aee7bd877151063bc6f762888
Jinwoo Jeong <mail@jinwoojeo.ng>
2026-08-10 03:13:51 +0900

    add clis

--- home/common.nix
+++ home/common.nix
@@ -4,13 +4,22 @@
   home.stateVersion = "26.05";
 
   home.packages = with pkgs; [
+    actionlint
+    cmake
     eza
+    ffmpeg
     ripgrep
     fzf
     gh
+    go
     jq
+    neovim
+    nodejs_24
     tmux
     zoxide
+    pnpm
+    tree-sitter
+    yarn
   ];
 
   home.file.".tmux.conf".source = ../tmux.conf;
