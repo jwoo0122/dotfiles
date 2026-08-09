4cef442da14da951ddb27fee8b0fc28569bf648f
Jinwoo Jeong <jinwoo.leo.jeong@icloud.com>
2026-08-10 03:26:05 +0900

    zshrc

--- home/common.nix
+++ home/common.nix
@@ -9,18 +9,47 @@
     eza
     ffmpeg
     ripgrep
-    fzf
+    # fzf
     gh
     go
     jq
     neovim
     nodejs_24
     tmux
-    zoxide
+    # zoxide
     pnpm
     tree-sitter
     yarn
   ];
+
+  programs.zsh = {
+    enable = true;
+
+    plugins = [
+      {
+        name = "zsh-autosuggestions";
+        src = pkgs.zsh-autosuggestions;
+        file = "share/zsh-autosuggestions/zsh-autosuggestions.zsh";
+      }
+      {
+        name = "zsh-syntax-highlighting";
+        src = pkgs.zsh-syntax-highlighting;
+        file = "share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh";
+      }
+    ];
+
+    initContent = builtins.readFile ../dot_zshrc;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
 
   home.file.".tmux.conf".source = ../tmux.conf;
 
